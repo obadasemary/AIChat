a6917832da0e10796049eb87b7707a29ea9264f1
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-04-08 00:04:08 +0300

    feat: Add WelcomeView UI

---
+++ AIChat/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 07.04.2025.
+//
+
+import Foundation
+
+struct Constants {
+    static let randomImage = "https://picsum.photos/600/600"
+    static let termsOfServiceURL = "https://github.com/obadasemary/AIChat/blob/main/LICENSE"
+    static let privacyPolicyURL = "https://github.com/obadasemary/AIChat/blob/main/LICENSE"
+}
