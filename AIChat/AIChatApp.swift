a9f2d6bb83ea6ca178da224ce689a74f65bac831
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-04-07 15:33:55 +0300

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 07.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatTests/AIChatTests.swift
@@ -0,0 +1,16 @@
+//
+//  AIChatTests.swift
+//  AIChatTests
+//
+//  Created by Abdelrahman Mohamed on 07.04.2025.
+//
+
+import Testing
+
+struct AIChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
