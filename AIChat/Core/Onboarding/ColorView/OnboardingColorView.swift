87012d3aa954a35cf4e71f2d53e14a41a6ef6743
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-04-08 15:48:21 +0300

    feat: OnboardingCompletedView

--- AIChat/Core/Onboarding/ColorView/OnboardingColorView.swift
+++ AIChat/Core/Onboarding/ColorView/OnboardingColorView.swift
@@ -37,7 +37,7 @@ struct OnboardingColorView: View {
             content: {
                 ZStack {
                     if let selectedColor {
-                        ctaButton
+                        ctaButton(selectedColor: selectedColor)
                             .transition(AnyTransition.move(edge: .bottom))
                     }
                 }
@@ -46,6 +46,7 @@ struct OnboardingColorView: View {
             }
         )
         .animation(.bouncy, value: selectedColor)
+        .toolbar(.hidden, for: .navigationBar)
     }
     
     private var colorGrid: some View {
@@ -83,9 +84,9 @@ struct OnboardingColorView: View {
         )
     }
     
-    private var ctaButton: some View {
+    private func ctaButton(selectedColor: Color) -> some View {
         NavigationLink {
-            OnboardingCompletedView()
+            OnboardingCompletedView(selectedColor: selectedColor)
         } label: {
             Text("Continue")
                 .callToActionButton()
@@ -97,4 +98,5 @@ struct OnboardingColorView: View {
     NavigationStack {
         OnboardingColorView()
     }
+    .environment(AppState())
 }
