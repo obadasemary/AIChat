d6702bb280e6719a13d12e693e3cccc81037cf28
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-04-10 21:01:47 +0300

    feat: ChatModel, ChatMessageModel

---
+++ AIChat/Services/Chat/Models/ChatModel.swift
@@ -0,0 +1,81 @@
+//
+//  ChatModel.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 10.04.2025.
+//
+
+import Foundation
+
+struct ChatModel: Identifiable {
+    
+    let id: String
+    let userId: String
+    let avatarId: String
+    let dateCreated: Date
+    let dateModified: Date
+    
+    init(
+        id: String,
+        userId: String,
+        avatarId: String,
+        dateCreated: Date,
+        dateModified: Date
+    ) {
+        self.id = id
+        self.userId = userId
+        self.avatarId = avatarId
+        self.dateCreated = dateCreated
+        self.dateModified = dateModified
+    }
+    
+    static var mock: ChatModel {
+        mocks[0]
+    }
+    
+    static var mocks: [ChatModel] {
+        let now = Date()
+        return [
+            ChatModel(
+                id: "mock_chat_1",
+                userId: "user_001",
+                avatarId: "avatar_001",
+                dateCreated: now,
+                dateModified: now
+            ),
+            ChatModel(
+                id: "chat_002",
+                userId: "user_002",
+                avatarId: "avatar_002",
+                dateCreated: now.addingTimeInterval(
+                    hours: -1
+                ),
+                dateModified: now.addingTimeInterval(
+                    minutes: -30
+                )
+            ),
+            ChatModel(
+                id: "chat_003",
+                userId: "user_003",
+                avatarId: "avatar_003",
+                dateCreated: now.addingTimeInterval(
+                    hours: -2
+                ),
+                dateModified: now.addingTimeInterval(
+                    hours: -1
+                )
+            ),
+            ChatModel(
+                id: "chat_004",
+                userId: "user_004",
+                avatarId: "avatar_004",
+                dateCreated: now.addingTimeInterval(
+                    days: -1
+                ),
+                dateModified: now.addingTimeInterval(
+                    hours: -10
+                )
+            )
+        ]
+    }
+}
