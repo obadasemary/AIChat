95f5e044f8fdbcbd1f62b0dd3d5bf3890f19c900
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-05-29 03:31:19 +0300

    chore: using `CreateAccountView`, and `onSignInPressed` into `WelcomeView`

--- AIChat/Core/Welcome/WelcomeView.swift
+++ AIChat/Core/Welcome/WelcomeView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct WelcomeView: View {
     
     @State var imageName: String = Constants.randomImage
+    @State private var showSignInView: Bool = false
     
     var body: some View {
         NavigationStack {
@@ -25,6 +26,13 @@ struct WelcomeView: View {
                 
                 policyLinks
             }
+        }
+        .sheet(isPresented: $showSignInView) {
+            CreateAccountView(
+                title: "Sign In",
+                subtitle: "Connect to an existing account"
+            )
+            .presentationDetents([.medium])
         }
     }
     
@@ -55,10 +63,13 @@ struct WelcomeView: View {
                 .padding(8)
                 .tappableBackground()
                 .onTapGesture {
-                    
+                    onSignInPressed()
                 }
-            
         }
+    }
+    
+    private func onSignInPressed() {
+        showSignInView = true
     }
     
     private var policyLinks: some View {
