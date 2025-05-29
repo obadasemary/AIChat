78ccf8e098c2ff404e47c9a4ec07d71f0dc520b8
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-05-29 03:29:28 +0300

    feat: implement `CreateAccountView`

---
+++ AIChat/Core/CreateAccount/CreateAccountView.swift
@@ -0,0 +1,45 @@
+//
+//  CreateAccountView.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 29.05.2025.
+//
+
+import SwiftUI
+
+struct CreateAccountView: View {
+    
+    var title: String = "Create Account"
+    var subtitle: String = "Don't lose your data! Connect to an SSO provider to save your account."
+    
+    var body: some View {
+        VStack(spacing: 24) {
+            VStack(alignment: .leading, spacing: 8) {
+                Text(title)
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                Text(subtitle)
+                    .font(.body)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            SignInWithAppleButtonView(
+                type: .signIn,
+                style: .black,
+                cornerRadius: 10
+            )
+            .frame(height: 55)
+            .anyButton(.press) {
+                
+            }
+            
+            Spacer()
+        }
+        .padding(16)
+        .padding(.top, 40)
+    }
+}
+
+#Preview {
+    CreateAccountView()
+}
