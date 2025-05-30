48c31e832b39a3ae8b8a552682b7bb5762beae78
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-05-30 22:49:31 +0300

    feat: init `CreateAvatarView`

---
+++ AIChat/Core/CreateAvatar/CreateAvatarView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateAvatarView.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 30.05.2025.
+//
+
+import SwiftUI
+
+struct CreateAvatarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateAvatarView()
+}
