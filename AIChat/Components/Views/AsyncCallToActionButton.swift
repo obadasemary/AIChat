1641f7b5a502b0daec24dda6befa3b120c9c0045
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-01 15:28:06 +0300

    feat: introduce `AsyncCallToActionButton`

---
+++ AIChat/Components/Views/AsyncCallToActionButton.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncCallToActionButton.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 01.06.2025.
+//
+
+import SwiftUI
+
+struct AsyncCallToActionButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncCallToActionButton()
+}
