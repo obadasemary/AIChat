74d610f71daea8b1bae79492789ec0a6eaaec483
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-02 23:43:41 +0300

    feat: implement `ChatBubbleViewBuilder`

---
+++ AIChat/Components/Views/ChatBubbleView/ChatBubbleViewBuilder.swift
@@ -0,0 +1,76 @@
+//
+//  ChatBubbleViewBuilder.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 02.06.2025.
+//
+
+import SwiftUI
+
+struct ChatBubbleViewBuilder: View {
+    
+    var message: ChatMessageModel = .mock
+    var isCurrentUser: Bool = false
+    var imageName: String?
+    
+    var body: some View {
+        ChatBubbleView(
+            text: message.content ?? "",
+            textColor: isCurrentUser ? .white: .primary,
+            backgroundColor: isCurrentUser ? .accent: Color(
+                uiColor: .systemGray6
+            ),
+            showImage: !isCurrentUser,
+            imageName: imageName
+        )
+        .frame(
+            maxWidth: .infinity,
+            alignment: isCurrentUser ? .trailing : .leading
+        )
+        .padding(.leading, isCurrentUser ? 75 : 0)
+        .padding(.trailing, isCurrentUser ? 0 : 75)
+    }
+}
+
+#Preview {
+    ScrollView {
+        VStack(spacing: 24) {
+            ChatBubbleViewBuilder(imageName: Constants.randomImage)
+            ChatBubbleViewBuilder(isCurrentUser: true)
+            ChatBubbleViewBuilder(
+                message: ChatMessageModel(
+                    id: UUID().uuidString,
+                    chatId: UUID().uuidString,
+                    authorId: UUID().uuidString,
+                    content: "Yes I am one of the best Error: this application, or a library it uses, has passed an invalid numeric value (NaN, or not-a-number) to CoreGraphics API and this value is being ignored. Please fix this problem.",
+                    seenByIds: nil,
+                    dateCreated: .now
+                ),
+                imageName: Constants.randomImage
+            )
+            ChatBubbleViewBuilder(
+                message: ChatMessageModel(
+                    id: UUID().uuidString,
+                    chatId: UUID().uuidString,
+                    authorId: UUID().uuidString,
+                    content: "Yes I am one of the best Error: this application, or a library it uses.",
+                    seenByIds: nil,
+                    dateCreated: .now
+                ),
+                imageName: Constants.randomImage
+            )
+            ChatBubbleViewBuilder(
+                message: ChatMessageModel(
+                    id: UUID().uuidString,
+                    chatId: UUID().uuidString,
+                    authorId: UUID().uuidString,
+                    content: "Yes I am one of the best Error: this application, or a library it uses, has passed an invalid numeric value (NaN, or not-a-number) to CoreGraphics API and this value is being ignored. Please fix this problem.",
+                    seenByIds: nil,
+                    dateCreated: .now
+                ),
+                isCurrentUser: true
+            )
+        }
+        .padding()
+    }
+}
