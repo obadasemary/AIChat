e396b374650dcc2821e65bf36af357439c96bca6
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-03 00:47:38 +0300

    feat: implement `textFieldSection`

--- AIChat/Core/Chat/ChatView.swift
+++ AIChat/Core/Chat/ChatView.swift
@@ -12,6 +12,7 @@ struct ChatView: View {
     @State private var chatMessages: [ChatMessageModel] = ChatMessageModel.mocks
     @State private var avatar: AvatarModel? = .mock
     @State private var currentUser: UserModel? = .mock
+    @State private var textFieldText: String = ""
     
     var body: some View {
         VStack(spacing: .zero) {
@@ -24,6 +25,7 @@ struct ChatView: View {
                             isCurrentUser: isCurrentUser,
                             imageName: nil
                         )
+            textFieldSection
                     }
                 }
                 .frame(maxWidth: .infinity)
@@ -35,6 +37,35 @@ struct ChatView: View {
         }
         .navigationTitle(avatar?.name ?? "Chat")
         .toolbarTitleDisplayMode(.inline)
+    private var textFieldSection: some View {
+        TextField("Type your message...", text: $textFieldText)
+            .keyboardType(.alphabet)
+            .autocorrectionDisabled()
+            .padding(12)
+            .padding(.trailing, 60)
+            .overlay(alignment: .trailing) {
+                Image(systemName: "arrow.up.circle.fill")
+                    .font(.system(size: 32))
+                    .padding(.trailing, 4)
+                    .foregroundStyle(.accent)
+                    .anyButton(.plain) {
+                        onSendMessageTapped()
+                    }
+                    .disabled(textFieldText.isEmpty)
+            }
+            .background {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 100)
+                        .fill(Color(uiColor: .systemBackground))
+                    
+                    RoundedRectangle(cornerRadius: 100)
+                        .stroke(Color.gray.opacity(0.3), lineWidth: 1)
+                }
+            }
+            .padding(.horizontal, 12)
+            .padding(.vertical, 6)
+            .background(Color(uiColor: .secondarySystemBackground))
+    }
     }
 }
 
