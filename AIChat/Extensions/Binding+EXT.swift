010cccce4aeb9e78978ed22ef957da7b2a31a2b1
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-03 20:19:38 +0300

    feat: implement `Binding+EXT`

---
+++ AIChat/Extensions/Binding+EXT.swift
@@ -0,0 +1,20 @@
+//
+//  Binding+EXT.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 03.06.2025.
+//
+
+import SwiftUI
+
+extension Binding where Value == Bool {
+    init<T: Sendable>(ifNotNil value: Binding<T?>) {
+        self.init {
+            value.wrappedValue != nil
+        } set: { newValue in
+            if !newValue {
+                value.wrappedValue = nil
+            }
+        }
+    }
+}
