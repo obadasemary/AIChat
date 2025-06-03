0a6790eb6ce80818462463c8a3d3a661fcbdc5dd
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-03 20:19:50 +0300

    feat: implement `TextValidationHelper`

---
+++ AIChat/Utilities/TextValidationHelper.swift
@@ -0,0 +1,38 @@
+//
+//  TextValidationError.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 03.06.2025.
+//
+
+import Foundation
+
+struct TextValidationHelper {
+    static func checkIfTextIsValid(text: String) throws {
+        let minimumCharactersCount: Int = 3
+        
+        guard text.count >= minimumCharactersCount else {
+            throw TextValidationError.notEnoughCharacters(min: minimumCharactersCount)
+        }
+        
+        let badWords: [String] = ["bad", "word"]
+        
+        if badWords.contains(where: text.lowercased().contains) {
+            throw TextValidationError.hasBadWords
+        }
+    }
+    
+    enum TextValidationError: LocalizedError {
+        case notEnoughCharacters(min: Int)
+        case hasBadWords
+        
+        var errorDescription: String? {
+            switch self {
+            case .notEnoughCharacters(min: let min):
+                return "Your message should contain at least \(min) characters."
+            case .hasBadWords:
+                return "Your message contains inappropriate words."
+            }
+        }
+    }
+}
