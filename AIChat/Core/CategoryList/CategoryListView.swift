fa6b1a19c98ffe789de12179cd82f15b6d5476f4
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-05 01:06:15 +0300

    feat: init `CategoryListView`

---
+++ AIChat/Core/CategoryList/CategoryListView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryListView.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 05.06.2025.
+//
+
+import SwiftUI
+
+struct CategoryListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryListView()
+}
