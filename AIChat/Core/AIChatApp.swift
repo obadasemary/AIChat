44aac106ea3691faa2a42d2dbce8415812df1109
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-11 04:48:13 +0300

    chore: implement `EnvironmentBuilderView` to wrap `FirebaseAuthService` environment into

--- AIChat/Core/AIChatApp.swift
+++ AIChat/Core/AIChatApp.swift
@@ -16,8 +16,20 @@ struct AIChatApp: App {
     
     var body: some Scene {
         WindowGroup {
-            AppView()
+            EnvironmentBuilderView {            
+                AppView()
+            }
         }
+    }
+}
+
+struct EnvironmentBuilderView<Content: View>: View {
+    
+    @ViewBuilder var content: () -> Content
+    
+    var body: some View {
+        content()
+            .environment(\.authService, FirebaseAuthService())
     }
 }
 
