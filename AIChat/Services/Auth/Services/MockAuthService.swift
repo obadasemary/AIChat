13e9ee0f1bbe5c52968496d83336429062c7bdcb
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-11 04:44:04 +0300

    feat: implement `MockAuthService`

---
+++ AIChat/Services/Auth/Services/MockAuthService.swift
@@ -0,0 +1,43 @@
+//
+//  MockAuthService.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 11.06.2025.
+//
+
+import Foundation
+
+struct MockAuthService {
+    
+    let currentUser: UserAuthInfo?
+    
+    init(currentUser: UserAuthInfo? = nil) {
+        self.currentUser = currentUser
+    }
+}
+
+extension MockAuthService: AuthServiceProtocol {
+    
+    func getAuthenticatedUser() -> UserAuthInfo? {
+        currentUser
+    }
+    
+    func signInAnonymously() async throws -> (user: UserAuthInfo, isNewUser: Bool) {
+        let user = UserAuthInfo.mock(isAnonymous: true)
+        return (user, true)
+    }
+    
+    func signInWithApple() async throws -> (user: UserAuthInfo, isNewUser: Bool) {
+        let user = UserAuthInfo.mock(isAnonymous: false)
+        return (user, false)
+    }
+    
+    func signInWithGoogle() async throws -> (user: UserAuthInfo, isNewUser: Bool) {
+        let user = UserAuthInfo.mock(isAnonymous: false)
+        return (user, false)
+    }
+    
+    func signOut() throws {}
+    
+    func deleteAccount() async throws {}
+}
