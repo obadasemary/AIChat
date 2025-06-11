d989cc1276def6831e9fd396ef836ef7f02bfbe5
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-11 04:42:30 +0300

    feat: make `GoogleSignInResult` as `safely mark it Sendable`

---
+++ AIChat/Utilities/GoogleSignInResult.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 11.06.2025.
+//
+
+import 
+
+// MARK: - Concurrency
+// GoogleSignInResult is only a pair of Strings, so we can safely mark it Sendable.
+extension GoogleSignInResult: @unchecked @retroactive Sendable {}
