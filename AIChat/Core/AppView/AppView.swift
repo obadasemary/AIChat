ce0a0b5ef4f12ab59bccd604bb7e89f978841cf5
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-13 18:36:40 +0300

    chore: implement userManager.login

--- AIChat/Core/AppView/AppView.swift
+++ AIChat/Core/AppView/AppView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct AppView: View {
     
     @Environment(AuthManager.self) private var authManager
+    @Environment(UserManager.self) private var userManager
     @State var appState: AppState = .init()
     
     var body: some View {
@@ -39,12 +40,23 @@ struct AppView: View {
         if let user = authManager.auth {
             // User is authenticated
             print("User already authenticated: \(user.uid)")
+            do {
+                try await userManager.logIn(auth: user, isNewUser: false)
+            } catch {
+                print("Failed to log in to auth for existing user: \(error)")
+                try? await Task.sleep(for: .seconds(5))
+                await checkUserStatus()
+            }
         } else {
             do {
                 let result = try await authManager.signInAnonymously()
                 print("Sign in anonymous success: \(result.user.uid)")
+                try await userManager
+                    .logIn(auth: result.user, isNewUser: result.isNewUser)
             } catch {
-                print(error)
+                print("Failed to sign in to annonimously and login: \(error)")
+                try? await Task.sleep(for: .seconds(5))
+                await checkUserStatus()
             }
         }
     }
