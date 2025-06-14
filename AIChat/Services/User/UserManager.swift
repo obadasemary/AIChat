ee7a1430b2c0b5bb03a8a7da80fc0426b5f3a3dd
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-14 19:02:26 +0300

    feat: implement `logIn`, `markOnboardingCompleteForCurrentUser`, `signOut`, and `deleteCurrentUser` into `UserManager`

--- AIChat/Services/User/UserManager.swift
+++ AIChat/Services/User/UserManager.swift
@@ -6,10 +6,14 @@
 //
 
 import Foundation
+import FirebaseFirestore
 
 protocol UserManagerProtocol: Sendable {
     
-    func logIn(auth: UserAuthInfo, isNewUser: Bool) async throws
+    @MainActor func logIn(auth: UserAuthInfo, isNewUser: Bool) async throws
+    @MainActor func markOnboardingCompleteForCurrentUser(profileColorHex: String) async throws
+    @MainActor func signOut()
+    @MainActor func deleteCurrentUser() async throws
 }
 
 @MainActor
@@ -18,6 +22,7 @@ final class UserManager {
     
     private let service: UserServiceProtocol
     private(set) var currentUser: UserModel?
+    private var currentUserListener: ListenerRegistration?
     
     init(service: UserServiceProtocol) {
         self.service = service
@@ -32,9 +37,56 @@ extension UserManager: UserManagerProtocol {
         let user = UserModel(auth: auth, creationVersion: creationVersion)
         
         try await service.saveUser(user: user)
+        addCurrentUserListener(userId: auth.uid)
+    }
+    
+    func markOnboardingCompleteForCurrentUser(profileColorHex: String) async throws {
+        let uId = try currentUserId()
+        try await service
+            .markOnboardingAsCompleted(
+                userId: uId,
+                profileColorHex: profileColorHex
+            )
+    }
+    
+    func signOut() {
+        currentUserListener?.remove()
+        currentUserListener = nil
+        currentUser = nil
+    }
+    
+    func deleteCurrentUser() async throws {
+        let uId = try currentUserId()
+        try await service.deleteUser(userId: uId)
+        signOut()
     }
 }
 
 private extension UserManager {
     
+    func addCurrentUserListener(userId: String) {
+        currentUserListener?.remove()
+        Task {
+            do {
+                for try await value in service.streamUser(userId: userId) {
+                    self.currentUser = value
+                    print("Successfully listened to user changes for userId: \(value.userId)")
+                }
+            } catch {
+                print("Error attaching user listener: \(error)")
+            }
+        }
+    }
+    
+    func currentUserId() throws -> String {
+        guard let uId = currentUser?.userId else {
+            throw UserManagerError.noUserId
+        }
+        
+        return uId
+    }
+    
+    enum UserManagerError: LocalizedError {
+        case noUserId
+    }
 }
