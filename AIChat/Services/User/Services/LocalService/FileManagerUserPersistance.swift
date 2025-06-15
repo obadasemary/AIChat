73a6ca1e39f67eded75c378c786e94a2767ac14e
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-15 06:56:10 +0300

    chore: inject `keychain helper` into `FileManagerUserPersistance`

--- AIChat/Services/User/Services/LocalService/FileManagerUserPersistance.swift
+++ AIChat/Services/User/Services/LocalService/FileManagerUserPersistance.swift
@@ -9,14 +9,38 @@ import Foundation
 
 struct FileManagerUserPersistance {
     private let userDocumentKey: String = "current_user"
+    private let keychain: KeychainHelperProtocol
+    
+    init(keychain: KeychainHelperProtocol) {
+        self.keychain = keychain
+    }
 }
 
 extension FileManagerUserPersistance: LocalUserServiceProtocol {
     func getCurrentUser() -> UserModel? {
-        try? FileManager.getDocument(key: userDocumentKey)
+        try? FileManager
+            .getDocument(key: userDocumentKey, password: encryptionPassword())
     }
     
     func saveCurrentUser(user: UserModel?) throws {
-        try FileManager.saveDocument(key: userDocumentKey, value: user)
+        try FileManager
+            .saveDocument(
+                key: userDocumentKey,
+                value: user,
+                password: encryptionPassword()
+            )
+    }
+}
+
+private extension FileManagerUserPersistance {
+    func encryptionPassword() -> String {
+        let keychainKey = "com.myapp.encryptionKey"
+        if let existing = try? keychain.get(keychainKey) {
+            return existing
+        } else {
+            let newKey = UUID().uuidString
+            try? keychain.set(newKey, for: keychainKey)
+            return newKey
+        }
     }
 }
