9a6671ec4733e4375ad672c4d316a6457bdec27c
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-15 20:17:46 +0300

    chore: update `generatedImage`

--- AIChat/Services/AI/Services/OpenAIServer.swift
+++ AIChat/Services/AI/Services/OpenAIServer.swift
@@ -22,13 +22,13 @@ extension OpenAIServer: AIServiceProtocol {
         let query = ImagesQuery(
             prompt: input,
             n: 1,
-            size: ._1024
+            size: ._512
         )
         
         let result = try await openAI.images(query: query)
         
         guard let b64Json = result.data.first?.b64Json,
-              let data = Data(base64Encoded: b64Json, options: .ignoreUnknownCharacters),
+              let data = Data(base64Encoded: b64Json),
               let image = UIImage(data: data) else {
             throw OpenAIError.invalidResponse
         }
