cf3c7c09adf894c30de8857d1cb0007caef5c4e7
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-17 01:31:00 +0300

    feat: implement `MockAvatarService`

--- AIChat/Services/Avatar/Services/MockAvatarService.swift
+++ AIChat/Services/Avatar/Services/MockAvatarService.swift
@@ -12,4 +12,24 @@ struct MockAvatarService {}
 extension MockAvatarService: AvatarServiceProtocol {
     
     func createAvatar(avatar: AvatarModel, image: UIImage) async throws {}
+    
+    func getFeaturedAvatars() async throws -> [AvatarModel] {
+        try await Task.sleep(for: .seconds(1))
+        return AvatarModel.mocks.shuffled()
+    }
+    
+    func getPopularAvatars() async throws -> [AvatarModel] {
+        try await Task.sleep(for: .seconds(2))
+        return AvatarModel.mocks.shuffled()
+    }
+    
+    func getAvatarsForCategory(category: CharacterOption) async throws -> [AvatarModel] {
+        try await Task.sleep(for: .seconds(2))
+        return AvatarModel.mocks.shuffled()
+    }
+    
+    func getAvatarsForAuthor(userId: String) async throws -> [AvatarModel] {
+        try await Task.sleep(for: .seconds(2))
+        return AvatarModel.mocks.shuffled()
+    }
 }
