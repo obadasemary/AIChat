fa380a2c774def7ad7e4f620b8334487b903ce00
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-17 01:30:32 +0300

    feat: implement `AvatarServiceProtocol`

--- AIChat/Services/Avatar/Services/AvatarServiceProtocol.swift
+++ AIChat/Services/Avatar/Services/AvatarServiceProtocol.swift
@@ -9,4 +9,8 @@ import SwiftUI
 
 protocol AvatarServiceProtocol: Sendable {
     func createAvatar(avatar: AvatarModel, image: UIImage) async throws
+    func getFeaturedAvatars() async throws -> [AvatarModel]
+    func getPopularAvatars() async throws -> [AvatarModel]
+    func getAvatarsForCategory(category: CharacterOption) async throws -> [AvatarModel]
+    func getAvatarsForAuthor(userId: String) async throws -> [AvatarModel]
 }
