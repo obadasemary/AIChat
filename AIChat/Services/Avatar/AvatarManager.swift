21d42400af9a3f7ad86859b81cc9ce6e61c9f68c
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-17 01:33:35 +0300

    feat: implement `AvatarManager`

--- AIChat/Services/Avatar/AvatarManager.swift
+++ AIChat/Services/Avatar/AvatarManager.swift
@@ -19,8 +19,25 @@ final class AvatarManager {
 }
 
 extension AvatarManager: AvatarManagerProtocol {
+
     func createAvatar(avatar: AvatarModel, image: UIImage) async throws {
         try await service.createAvatar(avatar: avatar, image: image)
+    }
+    
+    func getFeaturedAvatars() async throws -> [AvatarModel] {
+        try await service.getFeaturedAvatars()
+    }
+
+    func getPopularAvatars() async throws -> [AvatarModel] {
+        try await service.getPopularAvatars()
+    }
+    
+    func getAvatarsForCategory(category: CharacterOption) async throws -> [AvatarModel] {
+        try await service.getAvatarsForCategory(category: category)
+    }
+    
+    func getAvatarsForAuthor(userId: String) async throws -> [AvatarModel] {
+        try await service.getAvatarsForAuthor(userId: userId)
     }
 }
 
