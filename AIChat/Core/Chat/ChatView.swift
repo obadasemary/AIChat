a38115c1ed0fdf195233625409d89703dc5c91e1
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-21 04:23:06 +0300

    chore: implement `upload user chat` and `typingIndicatorMessage`

--- AIChat/Core/Chat/ChatView.swift
+++ AIChat/Core/Chat/ChatView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ChatView: View {
-
+    
     @Environment(UserManager.self) private var userManager
     @Environment(AuthManager.self) private var authManager
     @Environment(AvatarManager.self) private var avatarManager
@@ -26,6 +26,8 @@ struct ChatView: View {
     @State private var showAlert: AnyAppAlert?
     @State private var showChatSettings: AnyAppAlert?
     @State private var showProfileModal: Bool = false
+    @State private var isGeneratingResponse: Bool = false
+    @State private var typingIndicatorMessage: ChatMessageModel?
     
     var avatarId: String
     
@@ -38,11 +40,18 @@ struct ChatView: View {
         .toolbarTitleDisplayMode(.inline)
         .toolbar {
             ToolbarItem(placement: .navigationBarTrailing) {
-                Image(systemName: "ellipsis")
-                    .padding(8)
-                    .anyButton {
-                        onChatSettingsTapped()
+                HStack {
+                    if isGeneratingResponse {
+                        ProgressView()
                     }
+                    
+                    Image(systemName: "ellipsis")
+                        .padding(8)
+                        .anyButton {
+                            onChatSettingsTapped()
+                        }
+                    
+                }
             }
         }
         .showCustomAlert(type: .confirmationDialog, alert: $showChatSettings)
@@ -60,6 +69,11 @@ struct ChatView: View {
         }
     }
     
+    
+}
+
+// MARK: - load
+private extension ChatView {
     private func loadAvatar() async {
         do {
             avatar = try await avatarManager.getAvatar(id: avatarId)
@@ -74,10 +88,28 @@ struct ChatView: View {
         currentUser = userManager.currentUser
     }
     
+    
+    
+    private func profileModal(avatar: AvatarModel) -> some View {
+        ProfileModalView(
+            imageName: avatar.profileImageName,
+            title: avatar.name,
+            subtitle: avatar.characterOption?.rawValue.capitalized,
+            headline: avatar.characterDescription
+        ) {
+            showProfileModal = false
+        }
+        .padding()
+        .transition(.slide)
+    }
+}
+
+// MARK: - SectionViews
+private extension ChatView {
     private var scrollViewSection: some View {
         ScrollView {
             LazyVStack(spacing: 24) {
-                ForEach(chatMessages) { message in
+                ForEach(chatMessages + (typingIndicatorMessage.map { [$0] } ?? [])) { message in
                     let isCurrentUser = message.authorId == authManager.auth?.uid
                     ChatBubbleViewBuilder(
                         message: message,
@@ -89,7 +121,21 @@ struct ChatView: View {
                         }
                     )
                     .id(message.id)
+                    .transition(.opacity)
                 }
+                //                ForEach(chatMessages) { message in
+                //                    let isCurrentUser = message.authorId == authManager.auth?.uid
+                //                    ChatBubbleViewBuilder(
+                //                        message: message,
+                //                        isCurrentUser: isCurrentUser,
+                //                        currentUserProfileColor: currentUser?.profileColorCalculated ?? .accent,
+                //                        imageName: isCurrentUser ? nil : avatar?.profileImageName,
+                //                        onImageTapped: {
+                //                            onAvatarImageTapped()
+                //                        }
+                //                    )
+                //                    .id(message.id)
+                //                }
             }
             .frame(maxWidth: .infinity)
             .padding(8)
@@ -97,8 +143,10 @@ struct ChatView: View {
         }
         .rotationEffect(.degrees(180))
         .scrollPosition(id: $scrollPosition, anchor: .center)
-        .animation(.default, value: chatMessages.count)
-        .animation(.default, value: scrollPosition)
+        //        .animation(.default, value: chatMessages.count)
+        //        .animation(.default, value: scrollPosition)
+        .animation(.easeInOut, value: chatMessages.count)
+        .animation(.easeInOut, value: scrollPosition)
     }
     
     private var textFieldSection: some View {
@@ -130,67 +178,91 @@ struct ChatView: View {
             .padding(.vertical, 6)
             .background(Color(uiColor: .secondarySystemBackground))
     }
-    
-    private func profileModal(avatar: AvatarModel) -> some View {
-        ProfileModalView(
-            imageName: avatar.profileImageName,
-            title: avatar.name,
-            subtitle: avatar.characterOption?.rawValue.capitalized,
-            headline: avatar.characterDescription
-        ) {
-            showProfileModal = false
-        }
-        .padding()
-        .transition(.slide)
-    }
+}
+
+// MARK: - Action
+private extension ChatView {
     
     private func onSendMessageTapped() {
         guard !textFieldText.isEmpty else { return }
         
         let content = textFieldText
         
-        Task {        
+        Task {
             do {
+                // Get userId
                 let userId = try authManager.getAuthId()
+                
+                // Validate textField text
                 try TextValidationHelper.checkIfTextIsValid(text: content)
                 
+                // if chat is nil, then create a new chat
                 if chat == nil {
-                    let newChat = ChatModel.new(
-                        userId: userId,
-                        avatarId: avatarId
-                    )
-                    try await chatManager.createNewChat(chat: newChat)
-                    chat = newChat
+                    chat = try await createNewChat(userId: userId)
                 }
                 
-                let newChatMessage = AIChatModel(role: .user, message: content)
+                // if there is no chatm throw error (shold never happen)
+                guard let chat else {
+                    throw ChatViewEror.failedToCreateNewChat
+                }
                 
-                let chatId = UUID().uuidString
+                // create User Chat
+                let newChatMessage = AIChatModel(role: .user, message: content)
                 let message = ChatMessageModel.newUserMessage(
-                    chatId: chatId,
+                    chatId: chat.id,
                     userId: userId,
                     message: newChatMessage
                 )
                 
-                scrollPosition = message.id
-                
+                // upload user chat
+                try await chatManager
+                    .addChatMessage(
+                        message: message
+                    )
                 chatMessages.append(message)
+                
+                // clear text field & scroll to bottom
+                
+                // Show fake typing message
+                let typingMessage = ChatMessageModel(
+                    id: UUID().uuidString,
+                    chatId: chat.id,
+                    authorId: avatarId,
+                    content: AIChatModel(role: .assistant, message: "✍️..."),
+                    seenByIds: [],
+                    dateCreated: .now
+                )
+                typingIndicatorMessage = typingMessage
+                //                scrollPosition = message.id
+                scrollPosition = typingMessage.id
                 textFieldText = ""
                 
+                // Generate AI response
+                isGeneratingResponse = true
                 let aiChats = chatMessages.compactMap({ $0.content })
-                
                 let response = try await aiManager.generateText(chats: aiChats)
+                typingIndicatorMessage = nil
                 
+                // create AI Chat
                 let newAIMessage = ChatMessageModel.newUAIMessage(
-                    chatId: chatId,
+                    chatId: chat.id,
                     avatarId: avatarId,
                     message: response
                 )
+                
+                // upload AI chat
+                try await chatManager
+                    .addChatMessage(
+                        message: newAIMessage
+                    )
                 chatMessages.append(newAIMessage)
+                isGeneratingResponse = false
                 
             } catch let error {
                 showAlert = AnyAppAlert(error: error)
             }
+            
+            isGeneratingResponse = false
         }
     }
     
@@ -218,9 +290,48 @@ struct ChatView: View {
     }
 }
 
-#Preview {
+// MARK: - helper func
+private extension ChatView {
+    enum ChatViewEror: Error {
+        case failedToCreateNewChat
+    }
+    
+    private func createNewChat(userId: String) async throws -> ChatModel {
+        let newChat = ChatModel.new(
+            userId: userId,
+            avatarId: avatarId
+        )
+        try await chatManager.createNewChat(chat: newChat)
+        return newChat
+    }
+}
+
+#Preview("Working Chat") {
     NavigationStack {
         ChatView(avatarId: AvatarModel.mock.avatarId)
+            .previewEnvironment()
+    }
+}
+
+#Preview("Slow AI Generation") {
+    NavigationStack {
+        ChatView(avatarId: AvatarModel.mock.avatarId)
+            .environment(AIManager(service: MockAIServer(delay: 10)))
+            .previewEnvironment()
+    }
+}
+
+#Preview("Failed AI Generation") {
+    NavigationStack {
+        ChatView(avatarId: AvatarModel.mock.avatarId)
+            .environment(
+                AIManager(
+                    service: MockAIServer(
+                        delay: 2,
+                        showError: true
+                    )
+                )
+            )
             .previewEnvironment()
     }
 }
