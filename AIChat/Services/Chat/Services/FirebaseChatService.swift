c08858282af03e8ec5b24e1f6a4a02b618a5b836
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-21 04:21:14 +0300

    chore: implement `addChatMessage` as `FirebaseChatService`

--- AIChat/Services/Chat/Services/FirebaseChatService.swift
+++ AIChat/Services/Chat/Services/FirebaseChatService.swift
@@ -26,4 +26,22 @@ extension FirebaseChatService: ChatServiceProtocol {
             .document(chat.id)
             .setData(from: chat, merge: true)
     }
+    
+    func addChatMessage(message: ChatMessageModel) async throws {
+        try messageCollectionReference(for: message.chatId)
+            .document(message.id)
+            .setData(from: message, merge: true)
+        
+        try await collectionReference
+            .document(message.chatId).updateData([
+                ChatModel.CodingKeys.dateModified.rawValue: Date.now
+            ])
+    }
+}
+
+private extension FirebaseChatService {
+    
+    func messageCollectionReference(for chatId: String) -> CollectionReference {
+        collectionReference.document(chatId).collection("messages")
+    }
 }
