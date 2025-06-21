47ef90e1182cef93653779a0e4d87363f2a19cdd
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-21 03:05:50 +0300

    chore: add showError and delay to `MockAIServer` to control it later

--- AIChat/Services/AI/Services/MockAIServer.swift
+++ AIChat/Services/AI/Services/MockAIServer.swift
@@ -7,17 +7,47 @@
 
 import SwiftUI
 
-struct MockAIServer: AIServiceProtocol {
+struct MockAIServer {
+    
+    let delay: Double
+    let showError: Bool
+    
+    init(
+        delay: Double = 0.0,
+        showError: Bool = false
+    ) {
+        self.delay = delay
+        self.showError = showError
+    }
+}
+
+extension MockAIServer: AIServiceProtocol {
+    
     func generateImage(input: String) async throws -> UIImage {
-        try await Task.sleep(for: .seconds(3))
-        return UIImage(systemName: "gear")!
+        try await Task.sleep(for: .seconds(delay))
+        try tryShowError()
+        let (data, _) = try await URLSession.shared.data(from: URL(string: Constants.randomImage)!)
+        guard let image = UIImage(data: data) else {
+            return UIImage(systemName: "gear")!
+        }
+        
+        return image
     }
     
     func generateText(chats: [AIChatModel]) async throws -> AIChatModel {
-        try await Task.sleep(for: .seconds(2))
+        try await Task.sleep(for: .seconds(delay))
+        try tryShowError()
         return AIChatModel(
             role: .assistant,
             message: "This is returned text from the mock server AI."
         )
+    }
+}
+
+private extension MockAIServer {
+    func tryShowError() throws {
+        if showError {
+            throw URLError(.unknown)
+        }
     }
 }
