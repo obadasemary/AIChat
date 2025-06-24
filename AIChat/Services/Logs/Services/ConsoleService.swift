5ae3fd628efa437c1f1f4a3ac12140469505cc9d
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-24 22:09:23 +0300

    feat: implement Logger

---
+++ AIChat/Services/Logs/Services/ConsoleService.swift
@@ -0,0 +1,79 @@
+//
+//  ConsoleService.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 24.06.2025.
+//
+
+import Foundation
+
+struct ConsoleService {
+    
+    private let logger: LogSystem
+    private let printParameters: Bool
+    
+    init(
+        logger: LogSystem = LogSystem(),
+        printParameters: Bool = true
+    ) {
+        self.logger = logger
+        self.printParameters = printParameters
+    }
+}
+
+extension ConsoleService: LogServiceProtocol {
+    func identify(userId: String, name: String?, email: String?) {
+        let message = """
+📊 Identify User
+    userId: \(userId)
+    name: \(name ?? "unknown")
+    email: \(email ?? "unknown")
+"""
+        
+        logger.log(level: LogType.info, message: message)
+    }
+
+    func addUserProperty(dict: [String : Any]) {
+        var message = """
+📊 Log User Properties                
+"""
+        if printParameters {
+            let sortedKeys = dict.keys.sorted()
+            for key in sortedKeys {
+                if let value = dict[key] {
+                    message += "\n (key: \(key), value: \(value))"
+                }
+            }
+        }
+        
+        logger.log(level: LogType.info, message: message)
+    }
+
+    func deleteUserProfile() {
+        let message = """
+📊 Delete User Profile                
+"""
+        
+        logger.log(level: LogType.info, message: message)
+    }
+
+    func trackEvent(event: any LoggableEvent) {
+        var message = "\(event.type.emoji) \(event.eventName)"
+        
+        if printParameters, let parameters = event.parameters, !parameters.isEmpty {
+            
+            let sortedKeys = parameters.keys.sorted()
+            for key in sortedKeys {
+                if let value = parameters[key] {
+                    message += "\n (key: \(key), value: \(value))"
+                }
+            }
+        }
+        
+        logger.log(level: event.type, message: message)
+    }
+
+    func trackScreen(event: any LoggableEvent) {
+        trackEvent(event: event)
+    }
+}
---
+++ AIChat/Services/Logs/Services/LogSystem.swift
@@ -0,0 +1,64 @@
+//
+//  LogSystem.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 24.06.2025.
+//
+
+import Foundation
+import OSLog
+
+actor LogSystem {
+    
+    private let logger = Logger(
+        subsystem: Bundle.main.bundleIdentifier!,
+        category: "ConsoleLogger"
+    )
+    
+    func log(level: OSLogType, message: String) {
+        logger.log(level: level, "\(message)")
+    }
+    
+    nonisolated func log(level: LogType, message: String) {
+        Task {
+            await log(level: level.OsLogType, message: message)
+        }
+    }
+}
+
+enum LogType {
+    /// Use 'info' for informative tasks, These are not considered analytics, issues, or errors.
+    case info
+    /// Default type for analytics.
+    case analytic
+    /// Issues or errors that should not occur, but will not negatively affect the user experience.
+    case warning
+    /// Issues or errors that negatively affect user experience.
+    case severe
+    
+    var emoji: String {
+        switch self {
+        case .info:
+            "🔵"
+        case .analytic:
+            "📊"
+        case .warning:
+            "⚠️"
+        case .severe:
+            "🔴"
+        }
+    }
+    
+    var OsLogType: OSLogType {
+        switch self {
+        case .info:
+                .info
+        case .analytic:
+                .default
+        case .warning:
+                .error
+        case .severe:
+                .fault
+        }
+    }
+}
