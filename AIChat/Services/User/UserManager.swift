1e89078c0c96b104256f233fd2509923dc89e449
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-06-27 06:17:08 +0300

    chore: implement userManager Events

--- AIChat/Services/User/UserManager.swift
+++ AIChat/Services/User/UserManager.swift
@@ -13,12 +13,19 @@ final class UserManager {
     
     private let remoteService: RemoteUserServiceProtocol
     private let localStorage: LocalUserServiceProtocol
+    private let logManager: LogManagerProtocol?
+    
     private(set) var currentUser: UserModel?
     private var currentUserListener: ListenerRegistration?
     
-    init(services: UserServicesProtocol) {
+    init(
+        services: UserServicesProtocol,
+        logManager: LogManagerProtocol? = nil
+    ) {
         self.remoteService = services.remoteService
         self.localStorage = services.localStorage
+        self.logManager = logManager
+        
         self.currentUser = localStorage.getCurrentUser()
     }
 }
@@ -28,8 +35,21 @@ extension UserManager: UserManagerProtocol {
     func logIn(auth: UserAuthInfo, isNewUser: Bool) async throws {
         let creationVersion = isNewUser ? Utilities.appVersion : nil
         let user = UserModel(auth: auth, creationVersion: creationVersion)
+        logManager?
+            .trackEvent(
+                event: Event.logInStart(
+                    user: user
+                )
+            )
         
         try await remoteService.saveUser(user: user)
+        logManager?
+            .trackEvent(
+                event: Event.logInSuccess(
+                    user: user
+                )
+            )
+        
         addCurrentUserListener(userId: auth.uid)
     }
     
@@ -46,11 +66,16 @@ extension UserManager: UserManagerProtocol {
         currentUserListener?.remove()
         currentUserListener = nil
         currentUser = nil
+        logManager?.trackEvent(event: Event.signOut)
     }
     
     func deleteCurrentUser() async throws {
+        logManager?.trackEvent(event: Event.deleteAccountStart)
+        
         let uId = try currentUserId()
         try await remoteService.deleteUser(userId: uId)
+        logManager?.trackEvent(event: Event.deleteAccountSuccess)
+        
         signOut()
     }
 }
@@ -59,15 +84,33 @@ private extension UserManager {
     
     func addCurrentUserListener(userId: String) {
         currentUserListener?.remove()
+        logManager?.trackEvent(event: Event.remoteListenerStart)
+        
         Task {
             do {
                 for try await value in remoteService.streamUser(userId: userId) {
                     self.currentUser = value
+                    logManager?
+                        .trackEvent(
+                            event: Event.remoteListenerSuccess(
+                                user: value
+                            )
+                        )
+                    logManager?
+                        .addUserProperties(
+                            dict: value.eventParameters,
+                            isHighPriority: true
+                        )
+                        
                     saveCurrentUserLocally()
-                    print("Successfully listened to user changes for userId: \(value.userId)")
                 }
             } catch {
-                print("Error attaching user listener: \(error)")
+                logManager?
+                    .trackEvent(
+                        event: Event.remoteListenerFail(
+                            error: error
+                        )
+                    )
             }
         }
     }
@@ -81,12 +124,29 @@ private extension UserManager {
     }
     
     func saveCurrentUserLocally() {
+        logManager?
+            .trackEvent(
+                event: Event.saveLocalStart(
+                    user: currentUser
+                )
+            )
+        
         Task {
             do {
                 try localStorage.saveCurrentUser(user: currentUser)
-                print("Success saved current user locally")
+                logManager?
+                    .trackEvent(
+                        event: Event.saveLocalSuccess(
+                            user: currentUser
+                        )
+                    )
             } catch {
-                print("Error saving current user locally: \(error)")
+                logManager?
+                    .trackEvent(
+                        event: Event.saveLocalFail(
+                            error: error
+                        )
+                    )
             }
             
         }
@@ -94,5 +154,61 @@ private extension UserManager {
     
     enum UserManagerError: LocalizedError {
         case noUserId
+    }
+}
+
+// MARK: - Event
+private extension UserManager {
+    
+    enum Event: LoggableEvent {
+        case logInStart(user: UserModel?)
+        case logInSuccess(user: UserModel?)
+        case remoteListenerStart
+        case remoteListenerSuccess(user: UserModel?)
+        case remoteListenerFail(error: Error)
+        case saveLocalStart(user: UserModel?)
+        case saveLocalSuccess(user: UserModel?)
+        case saveLocalFail(error: Error)
+        case signOut
+        case deleteAccountStart
+        case deleteAccountSuccess
+        
+        
+        var eventName: String {
+            switch self {
+            case .logInStart: "UserMan_LogIn_Start"
+            case .logInSuccess: "UserMan_LogIn_Success"
+            case .remoteListenerStart: "UserMan_RemoteListener_Start"
+            case .remoteListenerSuccess: "UserMan_RemoteListener_Success"
+            case .remoteListenerFail: "UserMan_RemoteListener_Fail"
+            case .saveLocalStart: "UserMan_SaveLocal_Start"
+            case .saveLocalSuccess: "UserMan_SaveLocal_Success"
+            case .saveLocalFail: "UserMan_SaveLocal_Fail"
+            case .signOut: "UserMan_SignOut"
+            case .deleteAccountStart: "UserMan_DeleteAccount_Start"
+            case .deleteAccountSuccess: "UserMan_DeleteAccount_Success"
+                
+            }
+        }
+        
+        var parameters: [String: Any]? {
+            switch self {
+            case .logInStart(user: let user), .logInSuccess(user: let user), .remoteListenerSuccess(user: let user), .saveLocalStart(user: let user), .saveLocalSuccess(user: let user):
+                return user?.eventParameters
+            case .remoteListenerFail(error: let error), .saveLocalFail(error: let error):
+                return error.eventParameters
+            default:
+                return nil
+            }
+        }
+        
+        var type: LogType {
+            switch self {
+            case .remoteListenerFail, .saveLocalFail:
+                return .severe
+            default:
+                return .analytic
+            }
+        }
     }
 }
