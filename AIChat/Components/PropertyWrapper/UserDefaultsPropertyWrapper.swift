1c966815a687c5d217a910921def9e8da859fa1b
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-05 17:32:38 +0300

    feat: implement `UserDefaultsPropertyWrapper`

---
+++ AIChat/Components/PropertyWrapper/UserDefaultsPropertyWrapper.swift
@@ -0,0 +1,46 @@
+//
+//  UserDefaultsPropertyWrapper.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 05.07.2025.
+//
+
+import Foundation
+
+protocol UserDefaultsCompatible {  }
+extension Bool: UserDefaultsCompatible {  }
+extension Int: UserDefaultsCompatible {  }
+extension Float: UserDefaultsCompatible {  }
+extension Double: UserDefaultsCompatible {  }
+extension String: UserDefaultsCompatible {  }
+extension URL: UserDefaultsCompatible {  }
+
+@propertyWrapper
+struct UserDefault<T: UserDefaultsCompatible> {
+    
+    private let key: String
+    private let defaultValue: T
+    
+    init(
+        key: String,
+        defaultValue: T
+    ) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+    
+    var wrappedValue: T {
+        get {
+            if let savedValue = UserDefaults
+                .standard
+                .value(forKey: key) as? T {
+                return savedValue
+            } else {
+                UserDefaults.standard.set(defaultValue, forKey: key)
+                return defaultValue
+            }
+        } nonmutating set {
+            UserDefaults.standard.set(newValue, forKey: key)
+        }
+    }
+}
