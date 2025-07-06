bf70d6cd81399a0b032935e90fb2454ef94bd4aa
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-06 18:25:43 +0300

    feat: init `ABTestManagerProtocol`

---
+++ AIChat/Services/ABTests/ABTestManagerProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  ABTestManagerProtocol.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 06.07.2025.
+//
+
+import Foundation
+
+protocol ABTestManagerProtocol {
+    func override(updateTests: ActiveABTests) throws
+}
