2a8afc2f98099784b5df2452f712ee2e747d1645
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-07 19:16:13 +0300

    feat: implement `OnboardingCommunityView`

---
+++ AIChat/Core/Onboarding/CommunityView/OnboardingCommunityView.swift
@@ -0,0 +1,54 @@
+//
+//  OnboardingCommunityView.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 07.07.2025.
+//
+
+
+import SwiftUI
+
+struct OnboardingCommunityView: View {
+    var body: some View {
+        VStack {
+            VStack(spacing: 40) {
+                ImageLoaderView()
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                
+                Group {
+                    Text("Join our community with over ")
+                    +
+                    Text("1000+ ")
+                        .foregroundStyle(.accent)
+                        .fontWeight(.semibold)
+                    +
+                    Text("custom avatars. \nAsk them questions or have a casual conversation with them.")
+                }
+                .baselineOffset(6)
+                .minimumScaleFactor(0.5)
+                .padding(24)
+            }
+            .frame(maxHeight: .infinity)
+            
+            NavigationLink {
+                OnboardingColorView()
+            } label: {
+                Text("Continue")
+                    .callToActionButton()
+            }
+        }
+        .padding(24)
+        .font(.title3)
+        .toolbar(.hidden, for: .navigationBar)
+        .screenAppearAnalytics(name: "OnboardingCommunityView")
+    }
+}
+
+#Preview {
+    NavigationStack {
+        OnboardingCommunityView()
+    }
+    .environment(AppState())
+    .previewEnvironment()
+}
