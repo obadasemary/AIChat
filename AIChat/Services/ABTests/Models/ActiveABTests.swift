02562ea45decd1bd25d63eb1accea938f61d556a
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-08 00:47:18 +0300

    chore: add `categoryRowTest` into `ActiveABTests`

--- AIChat/Services/ABTests/Models/ActiveABTests.swift
+++ AIChat/Services/ABTests/Models/ActiveABTests.swift
@@ -11,24 +11,29 @@ struct ActiveABTests: Codable {
     
     private(set) var createAccountTest: Bool
     private(set) var onboardingCommunityTest: Bool
+    private(set) var categoryRowTest: CategoryRowTestOption
     
     init(
         createAccountTest: Bool,
-        onboardingCommunityTest: Bool
+        onboardingCommunityTest: Bool,
+        categoryRowTest: CategoryRowTestOption
     ) {
         self.createAccountTest = createAccountTest
         self.onboardingCommunityTest = onboardingCommunityTest
+        self.categoryRowTest = categoryRowTest
     }
     
     enum CodingKeys: String, CodingKey {
         case createAccountTest = "_20250702_CreateAccTest"
         case onboardingCommunityTest = "_20250702_OnbCommunityTest"
+        case categoryRowTest = "_20250702_CateegoryRowTest"
     }
     
     var eventParameters: [String: Any] {
         let dict: [String: Any?] = [
             "test\(CodingKeys.createAccountTest.rawValue)": createAccountTest,
-            "test\(CodingKeys.onboardingCommunityTest.rawValue)": onboardingCommunityTest
+            "test\(CodingKeys.onboardingCommunityTest.rawValue)": onboardingCommunityTest,
+            "test\(CodingKeys.categoryRowTest.rawValue)": categoryRowTest.rawValue
         ]
         return dict.compactMapValues { $0 }
     }
@@ -39,5 +44,19 @@ struct ActiveABTests: Codable {
     
     mutating func update(onboardingCommunityTest newValue: Bool) {
         onboardingCommunityTest = newValue
+    }
+    
+    mutating func update(categoryRowTest newValue: CategoryRowTestOption) {
+        categoryRowTest = newValue
+    }
+}
+
+enum CategoryRowTestOption: String, Codable, CaseIterable {
+    case original
+    case top
+    case hidden
+    
+    static var `default`: Self {
+        .original
     }
 }
