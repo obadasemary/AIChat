27906bbd9bb33b1c052b55314ff27df4f83b4163
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-07 19:16:33 +0300

    chore: add `onboardingCommunityTest` into `ActiveABTests`

--- AIChat/Services/ABTests/Models/ActiveABTests.swift
+++ AIChat/Services/ABTests/Models/ActiveABTests.swift
@@ -10,23 +10,34 @@ import Foundation
 struct ActiveABTests: Codable {
     
     private(set) var createAccountTest: Bool
+    private(set) var onboardingCommunityTest: Bool
     
-    init(createAccountTest: Bool) {
+    init(
+        createAccountTest: Bool,
+        onboardingCommunityTest: Bool
+    ) {
         self.createAccountTest = createAccountTest
+        self.onboardingCommunityTest = onboardingCommunityTest
     }
     
     enum CodingKeys: String, CodingKey {
-        case createAccountTest = "_20250702_createAccTest"
+        case createAccountTest = "_20250702_CreateAccTest"
+        case onboardingCommunityTest = "_20250702_OnbCommunityTest"
     }
     
     var eventParameters: [String: Any] {
         let dict: [String: Any?] = [
-            "test\(CodingKeys.createAccountTest.rawValue)": createAccountTest
+            "test\(CodingKeys.createAccountTest.rawValue)": createAccountTest,
+            "test\(CodingKeys.onboardingCommunityTest.rawValue)": onboardingCommunityTest
         ]
         return dict.compactMapValues { $0 }
     }
     
     mutating func update(createAccountTest newValue: Bool) {
         createAccountTest = newValue
+    }
+    
+    mutating func update(onboardingCommunityTest newValue: Bool) {
+        onboardingCommunityTest = newValue
     }
 }
