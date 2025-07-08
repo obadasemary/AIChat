6592ecf5b9e555d5a311d9d3d378e9b8ca679e41
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-08 16:13:02 +0300

    feat: implement `FirebaseABTestService`

---
+++ AIChat/Services/ABTests/Services/FirebaseABTestService.swift
@@ -0,0 +1,59 @@
+//
+//  FirebaseABTestService.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 08.07.2025.
+//
+
+import Foundation
+import FirebaseRemoteConfig
+
+@MainActor
+class FirebaseABTestService {
+    var activeTests: ActiveABTests {
+        ActiveABTests(config: RemoteConfig.remoteConfig())
+    }
+    
+    init() {
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 0
+        RemoteConfig.remoteConfig().configSettings = settings
+        
+        let defaultValue = ActiveABTests(
+            createAccountTest: false,
+            onboardingCommunityTest: false,
+            categoryRowTest: .default
+        )
+        
+        RemoteConfig
+            .remoteConfig()
+            .setDefaults(defaultValue.asNSObjectDictionary)
+        RemoteConfig
+            .remoteConfig()
+            .activate()
+    }
+}
+
+extension FirebaseABTestService: ABTestServiceProtocol {
+
+    func saveUpdatedConfig(updatedTest: ActiveABTests) throws {
+        assertionFailure("Error: Firebase AB Test are not configurable from the client.")
+    }
+    
+    func fetchUpdateConfig() async throws -> ActiveABTests {
+        let status = try await RemoteConfig.remoteConfig().fetchAndActivate()
+        
+        switch status {
+        case .successFetchedFromRemote, .successUsingPreFetchedData:
+            return activeTests
+        case .error:
+            throw RemoteConfigError.failedToFetch
+        @unknown default:
+            throw RemoteConfigError.failedToFetch
+        }
+    }
+    
+    enum RemoteConfigError: LocalizedError {
+        case failedToFetch
+    }
+}
