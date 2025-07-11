312aa01aa43381183107422bd6519c650727ed7f
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-11 17:52:25 +0300

    feat: implement `AnyProduct+StoreKit`

---
+++ AIChat/Services/Purchases/Models/AnyProduct+StoreKit.swift
@@ -0,0 +1,43 @@
+//
+//  AnyProduct+StoreKit.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 11.07.2025.
+//
+
+import StoreKit
+
+public extension AnyProduct {
+    
+    init(storeKitProduct product: StoreKit.Product) {
+        self.init(
+            id: product.id,
+            title: product.displayName,
+            subtitle: product.description,
+            priceString: product.displayPrice,
+            productDuration: ProductDurationOption(unit: product.subscription?.subscriptionPeriod.unit)
+        )
+    }
+}
+
+extension ProductDurationOption {
+    
+    init?(unit: Product.SubscriptionPeriod.Unit?) {
+        if let unit {
+            switch unit {
+            case .day:
+                self = .day
+            case .week:
+                self = .week
+            case .month:
+                self = .month
+            case .year:
+                self = .year
+            default:
+                return nil
+            }
+        } else {
+            return nil
+        }
+    }
+}
