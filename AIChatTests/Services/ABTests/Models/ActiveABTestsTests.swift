0fb7bae9dc947c419cd73550b56dbc4448f1b62c
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-15 22:33:13 +0300

    feat: init `ActiveABTestsTests`

---
+++ AIChatTests/Services/ABTests/Models/ActiveABTestsTests.swift
@@ -0,0 +1,16 @@
+//
+//  ActiveABTestsTests.swift
+//  AIChatTests
+//
+//  Created by Abdelrahman Mohamed on 14.07.2025.
+//
+
+import Testing
+
+struct ActiveABTestsTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
