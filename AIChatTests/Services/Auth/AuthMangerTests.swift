7c92122a49cf9ce934bf7b3ff52d6ab64bc5b89c
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-17 23:07:23 +0300

    feat: init `AuthMangerTests`

---
+++ AIChatTests/Services/Auth/AuthMangerTests.swift
@@ -0,0 +1,16 @@
+//
+//  AuthMangerTests.swift
+//  AIChatTests
+//
+//  Created by Abdelrahman Mohamed on 16.07.2025.
+//
+
+import Testing
+
+struct AuthMangerTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
