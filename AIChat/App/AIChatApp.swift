9f3fd9bdce65aa461e1815855d62e877db113dcf
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-22 01:40:03 +0300

    chore: clean `AIChatApp`

--- AIChat/App/AIChatApp.swift
+++ AIChat/App/AIChatApp.swift
@@ -8,42 +8,28 @@
 import SwiftUI
 import SwiftfulUtilities
 
-@main
-struct AppEntryPoint {
-    
-    static func main() {
-        if Utilities.isUnitTesting {
-            TestingApp.main()
-        } else {
-            AIChatApp.main()
-        }
-    }
-}
-
 struct AIChatApp: App {
     
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
-            AppView()
-                .environment(delegate.dependencies.purchaseManager)
-                .environment(delegate.dependencies.abTestManager)
-                .environment(delegate.dependencies.pushManager)
-                .environment(delegate.dependencies.chatManager)
-                .environment(delegate.dependencies.aiManager)
-                .environment(delegate.dependencies.avatarManager)
-                .environment(delegate.dependencies.userManager)
-                .environment(delegate.dependencies.authManager)
-                .environment(delegate.dependencies.logManager)
-        }
-    }
-}
-
-struct TestingApp: App {
-    var body: some Scene {
-        WindowGroup {
-            Text("Testing")
+            Group {
+                if Utilities.isUITesting {
+                    AppViewForUITesting()
+                } else {
+                    AppView()
+                }
+            }
+            .environment(delegate.dependencies.purchaseManager)
+            .environment(delegate.dependencies.abTestManager)
+            .environment(delegate.dependencies.pushManager)
+            .environment(delegate.dependencies.chatManager)
+            .environment(delegate.dependencies.aiManager)
+            .environment(delegate.dependencies.avatarManager)
+            .environment(delegate.dependencies.userManager)
+            .environment(delegate.dependencies.authManager)
+            .environment(delegate.dependencies.logManager)
         }
     }
 }
