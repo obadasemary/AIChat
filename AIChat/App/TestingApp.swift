04fbb86178551a004067c7fe125dab6f134635b0
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-22 01:39:22 +0300

    chore: implement `TestingApp`

---
+++ AIChat/App/TestingApp.swift
@@ -0,0 +1,16 @@
+//
+//  TestingApp.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 22.07.2025.
+//
+
+import SwiftUI
+
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Testing")
+        }
+    }
+}
