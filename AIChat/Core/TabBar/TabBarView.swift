450105e11259a7d609ac4e06d0eb4712b3955aa2
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-22 19:58:07 +0300

    chore: pass `container` via `ProfileViewModel` into `TabBarView` instead

--- AIChat/Core/TabBar/TabBarView.swift
+++ AIChat/Core/TabBar/TabBarView.swift
@@ -9,11 +9,7 @@ import SwiftUI
 
 struct TabBarView: View {
     
-    @Environment(AuthManager.self) private var authManager
-    @Environment(UserManager.self) private var userManager
-    @Environment(AvatarManager.self) private var avatarManager
-    @Environment(LogManager.self) private var logManager
-    @Environment(AIManager.self) private var aiManager
+    @Environment(DIContainer.self) private var container
     
     var body: some View {
         TabView {
@@ -29,13 +25,7 @@ struct TabBarView: View {
                     )
                 }
             ProfileView(
-                viewModel: ProfileViewModel(
-                    authManager: authManager,
-                    userManager: userManager,
-                    avatarManager: avatarManager,
-                    logManager: logManager,
-                    aiManager: aiManager
-                )
+                viewModel: ProfileViewModel(container: container)
             )
             .tabItem {
                 Label("Profile", systemImage: "person.fill")
