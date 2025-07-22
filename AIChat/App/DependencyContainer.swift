3e1e4c0cdb96aa8587142e9404e7a7a460aa6ef2
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-22 19:54:26 +0300

    feat: implement `DIContainer`

---
+++ AIChat/App/DependencyContainer.swift
@@ -0,0 +1,30 @@
+//
+//  DependencyContainer.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 22.07.2025.
+//
+
+import Foundation
+
+@Observable
+@MainActor
+class DIContainer {
+    
+    private var services: [String: Any] = [:]
+    
+    func register<T>(_ service: T.Type, _ implementation: T) {
+        let serviceName = String(describing: service)
+        services[serviceName] = implementation
+    }
+    
+    func register<T>(_ service: T.Type, _ implementation: () -> T) {
+        let serviceName = String(describing: service)
+        services[serviceName] = implementation()
+    }
+    
+    func resolve<T>(_ service: T.Type) -> T? {
+        let serviceName = String(describing: service)
+        return services[serviceName] as? T
+    }
+}
