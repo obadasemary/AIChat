d94a3af0ade8ba4b97049b223d3769058bf9dcc0
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-23 18:42:03 +0300

    feat: init `ProfileViewTests`

---
+++ AIChatTests/Core/Profile/ProfileViewTests.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewTests.swift
+//  AIChatTests
+//
+//  Created by Abdelrahman Mohamed on 23.07.2025.
+//
+
+import Testing
+
+struct ProfileViewTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
