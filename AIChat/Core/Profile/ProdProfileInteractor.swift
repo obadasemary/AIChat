3c2d52f483f7010657f3db77f52935915d000e00
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-24 23:13:21 +0300

    feat: implement `ProdProfileInteractor`

---
+++ AIChat/Core/Profile/ProdProfileInteractor.swift
@@ -0,0 +1,47 @@
+//
+//  ProdProfileInteractor.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 24.07.2025.
+//
+
+import Foundation
+
+@MainActor
+struct ProdProfileInteractor {
+    
+    private let authManager: AuthManager
+    private let userManager: UserManager
+    private let avatarManager: AvatarManager
+    private let logManager: LogManager
+    
+    init(container: DependencyContainer) {
+        self.authManager = container.resolve(AuthManager.self)!
+        self.userManager = container.resolve(UserManager.self)!
+        self.avatarManager = container.resolve(AvatarManager.self)!
+        self.logManager = container.resolve(LogManager.self)!
+    }
+}
+
+extension ProdProfileInteractor: ProfileInteractor {
+    
+    var currentUser: UserModel? {
+        userManager.currentUser
+    }
+    
+    func getAuthId() throws -> String {
+        try authManager.getAuthId()
+    }
+    
+    func getAvatarsForAuthor(userId: String) async throws -> [AvatarModel] {
+        try await avatarManager.getAvatarsForAuthor(userId: userId)
+    }
+    
+    func removeAuthorIdFromAvatar(avatarId: String) async throws {
+        try await avatarManager.removeAuthorIdFromAvatar(avatarId: avatarId)
+    }
+    
+    func trackEvent(event: any LoggableEvent) {
+        logManager.trackEvent(event: event)
+    }
+}
