fd08dee9ceb0829fc28ff25a4441255594fdb30d
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-24 23:13:08 +0300

    feat: implement `ProfileInteractor`

---
+++ AIChat/Core/Profile/ProfileInteractor.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileInteractor.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 24.07.2025.
+//
+
+import Foundation
+
+@MainActor
+protocol ProfileInteractor {
+    var currentUser: UserModel? { get }
+    
+    func getAuthId() throws -> String
+    func getAvatarsForAuthor(userId: String) async throws -> [AvatarModel]
+    func removeAuthorIdFromAvatar(avatarId: String) async throws
+    
+    func trackEvent(event: any LoggableEvent)
+}
