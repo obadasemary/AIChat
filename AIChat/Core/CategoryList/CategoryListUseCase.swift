c8b9c447135f82ad8e40e97b4921195e9c7ffb3c
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-28 01:50:10 +0300

    feat: implement `CategoryListUseCase`

---
+++ AIChat/Core/CategoryList/CategoryListUseCase.swift
@@ -0,0 +1,31 @@
+//
+//  CategoryListUseCase.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 28.07.2025.
+//
+
+import Foundation
+
+@MainActor
+final class CategoryListUseCase {
+    
+    private let avatarManager: AvatarManager
+    private let logManager: LogManager
+    
+    init(container: DependencyContainer) {
+        self.avatarManager = container.resolve(AvatarManager.self)!
+        self.logManager = container.resolve(LogManager.self)!
+    }
+}
+
+extension CategoryListUseCase: CategoryListUseCaseProtocol {
+    
+    func getAvatarsForCategory(category: CharacterOption) async throws -> [AvatarModel] {
+        try await avatarManager.getAvatarsForCategory(category: category)
+    }
+    
+    func trackEvent(event: any LoggableEvent) {
+        logManager.trackEvent(event: event)
+    }
+}
