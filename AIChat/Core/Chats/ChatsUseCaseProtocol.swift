587be23f214fca7a4c356da7709bf102f7fa23c7
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-28 01:29:34 +0300

    chore: remove unused func from `ChatsUseCaseProtocol`

--- AIChat/Core/Chats/ChatsUseCaseProtocol.swift
+++ AIChat/Core/Chats/ChatsUseCaseProtocol.swift
@@ -12,7 +12,5 @@ protocol ChatsUseCaseProtocol {
     func getAuthId() async throws -> String
     func getRecentAvatars() throws -> [AvatarModel]
     func getAllChats(userId: String) async throws -> [ChatModel]
-    func getAvatar(id: String) async throws -> AvatarModel?
-    func getLastChatMessage(chatId: String) async throws -> ChatMessageModel?
     func trackEvent(event: any LoggableEvent)
 }
