e5d8e3bd9891592b572823039a9d2a6285eb28f5
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-28 22:24:30 +0300

    chore: make `UserManagerProtocol` in `@MainActor`

--- AIChat/Services/User/UserManagerProtocol.swift
+++ AIChat/Services/User/UserManagerProtocol.swift
@@ -7,10 +7,11 @@
 
 import Foundation
 
+@MainActor
 protocol UserManagerProtocol: Sendable {
     
-    @MainActor func logIn(auth: UserAuthInfo, isNewUser: Bool) async throws
-    @MainActor func markOnboardingCompleteForCurrentUser(profileColorHex: String) async throws
-    @MainActor func signOut()
-    @MainActor func deleteCurrentUser() async throws
+    func logIn(auth: UserAuthInfo, isNewUser: Bool) async throws
+    func markOnboardingCompleteForCurrentUser(profileColorHex: String) async throws
+    func signOut()
+    func deleteCurrentUser() async throws
 }
