4b976ba92fd34e52d8430e17bafe65496a09f5d5
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-29 00:06:08 +0300

    feat: implement `OnboardingCompletedViewModel`

---
+++ AIChat/Core/Onboarding/CompletedView/OnboardingCompletedViewModel.swift
@@ -0,0 +1,98 @@
+//
+//  OnboardingCompletedViewModel.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 29.07.2025.
+//
+
+import SwiftUI
+
+@Observable
+@MainActor
+class OnboardingCompletedViewModel {
+    
+    private let onboardingCompletedUseCase: OnboardingCompletedUseCaseProtocol
+    
+    private(set) var isCompletingProfileSetup: Bool = false
+    
+    var showAlert: AnyAppAlert?
+    
+    init(onboardingCompletedUseCase: OnboardingCompletedUseCaseProtocol) {
+        self.onboardingCompletedUseCase = onboardingCompletedUseCase
+    }
+}
+
+// MARK: - Action
+extension OnboardingCompletedViewModel {
+    
+    func onFinishButtonPressed(selectedColor: Color, onShowTabBarView: @escaping () -> Void) {
+        isCompletingProfileSetup = true
+        onboardingCompletedUseCase.trackEvent(event: Event.finishStart)
+        Task {
+            do {
+                let hex = selectedColor.asHex()
+                try await onboardingCompletedUseCase
+                    .markOnboardingCompleteForCurrentUser(
+                        profileColorHex: hex
+                    )
+                onboardingCompletedUseCase
+                    .trackEvent(
+                        event: Event.finishSuccess(
+                            hex: hex
+                        )
+                    )
+                
+                isCompletingProfileSetup = false
+                onShowTabBarView()
+            } catch {
+                showAlert = AnyAppAlert(error: error)
+                onboardingCompletedUseCase
+                    .trackEvent(
+                        event: Event.finishFail(
+                            error: error
+                        )
+                    )
+            }
+        }
+    }
+}
+
+// MARK: - Event
+private extension OnboardingCompletedViewModel {
+    
+    enum Event: LoggableEvent {
+        case finishStart
+        case finishSuccess(hex: String)
+        case finishFail(error: Error)
+        
+        var eventName: String {
+            switch self {
+            case .finishStart: "OnboardingCompletedView_Finish_Start"
+            case .finishSuccess: "OnboardingCompletedView_Finish_Success"
+            case .finishFail: "OnboardingCompletedView_Finish_Fail"
+            }
+        }
+        
+        var parameters: [String: Any]? {
+            switch self {
+            case .finishSuccess(hex: let hex):
+                return [
+                    "profile_color_hex": hex
+                ]
+            case .finishFail(error: let error):
+                return error.eventParameters
+            default:
+                return nil
+            }
+        }
+        
+        var type: LogType {
+            switch self {
+            case .finishFail:
+                return .severe
+            default:
+                return .analytic
+            }
+        }
+    }
+}
