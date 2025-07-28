d04804c481ad5139fbf833d1cf2ef5cff255ac3f
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-28 19:24:12 +0300

    feat: implement `DevSettingsUseCase`

---
+++ AIChat/Core/DevSettings/DevSettingsUseCase.swift
@@ -0,0 +1,41 @@
+//
+//  DevSettingsUseCase.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 28.07.2025.
+//
+
+import Foundation
+
+@MainActor
+final class DevSettingsUseCase {
+    
+    private let authManager: AuthManager
+    private let userManager: UserManager
+    private let abTestManager: ABTestManager
+    
+    init(container: DependencyContainer) {
+        self.authManager = container.resolve(AuthManager.self)!
+        self.userManager = container.resolve(UserManager.self)!
+        self.abTestManager = container.resolve(ABTestManager.self)!
+    }
+}
+
+extension DevSettingsUseCase: DevSettingsUseCaseProtocol {
+    
+    var auth: UserAuthInfo? {
+        authManager.auth
+    }
+    
+    var currentUser: UserModel? {
+        userManager.currentUser
+    }
+    
+    var activeTests: ActiveABTests {
+        abTestManager.activeTests
+    }
+    
+    func override(updateTests: ActiveABTests) throws {
+        try abTestManager.override(updateTests: updateTests)
+    }
+}
