29a424635fbaeda9ae931d17f0db9e692db7cef6
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-28 19:52:55 +0300

    feat: implement `PaywallUseCaseProtocol`

---
+++ AIChat/Core/Paywall/PaywallUseCaseProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  PaywallUseCaseProtocol.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 28.07.2025.
+//
+
+import Foundation
+
+@MainActor
+protocol PaywallUseCaseProtocol {
+//    var paywallTest: PaywallTestOption { get }
+    func getProducts(productIds: [String]) async throws -> [AnyProduct]
+    func restorePurchase() async throws -> [PurchasedEntitlement]
+    func purchaseProduct(productId: String) async throws -> [PurchasedEntitlement]
+    func trackEvent(event: any LoggableEvent)
+}
