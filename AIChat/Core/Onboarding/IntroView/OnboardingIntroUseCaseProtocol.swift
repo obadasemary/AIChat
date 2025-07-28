84e3cbbf48f55e940d37269508e19b4dfaf629d8
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-29 00:07:30 +0300

    feat: implement `OnboardingIntroUseCaseProtocol`

---
+++ AIChat/Core/Onboarding/IntroView/OnboardingIntroUseCaseProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingIntroUseCaseProtocol.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 29.07.2025.
+//
+
+import Foundation
+
+@MainActor
+protocol OnboardingIntroUseCaseProtocol {
+    var onboardingCommunityTest: Bool { get }
+}
