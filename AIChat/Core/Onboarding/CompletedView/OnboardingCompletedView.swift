4a7414cdc227e977f3b9d6d6d209b565e0b61c23
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-29 00:06:34 +0300

    chore: In `OnboardingCompletedView` using `OnboardingCompletedViewModel` with `OnboardingCompletedUseCase` and pass `container` instead

--- AIChat/Core/Onboarding/CompletedView/OnboardingCompletedView.swift
+++ AIChat/Core/Onboarding/CompletedView/OnboardingCompletedView.swift
@@ -9,13 +9,10 @@ import SwiftUI
 
 struct OnboardingCompletedView: View {
     
-    @Environment(AppState.self) private var root
-    @Environment(UserManager.self) private var userManager
-    @Environment(LogManager.self) private var logManager
+    @Environment(AppState.self) private var appState
+    @State var viewModel: OnboardingCompletedViewModel
     
-    @State private var isCompletingProfileSetup: Bool = false
     var selectedColor: Color = .accent
-    @State private var showAlert: AnyAppAlert?
     
     var body: some View {
         VStack(alignment: .leading, spacing: 12) {
@@ -36,9 +33,16 @@ struct OnboardingCompletedView: View {
             spacing: 16,
             content: {
                 AsyncCallToActionButton(
-                    isLoading: isCompletingProfileSetup,
+                    isLoading: viewModel.isCompletingProfileSetup,
                     title: "Finish",
-                    action: onFinishButtonPressed
+                    action: {
+                        viewModel
+                            .onFinishButtonPressed(
+                                selectedColor: selectedColor
+                            ) {
+                                appState.updateViewState(showTabBarView: true)
+                            }
+                    }
                 )
                 .accessibilityIdentifier("FinishButton")
             }
@@ -46,88 +50,18 @@ struct OnboardingCompletedView: View {
         .padding(24)
         .toolbar(.hidden, for: .navigationBar)
         .screenAppearAnalytics(name: "OnboardingCompletedView")
-        .showCustomAlert(alert: $showAlert)
-    }
-}
-
-// MARK: - Action
-private extension OnboardingCompletedView {
-    
-    func onFinishButtonPressed() {
-        isCompletingProfileSetup = true
-        logManager.trackEvent(event: Event.finishStart)
-        Task {
-            do {
-                let hex = selectedColor.asHex()
-                try await userManager
-                    .markOnboardingCompleteForCurrentUser(
-                        profileColorHex: hex
-                    )
-                logManager
-                    .trackEvent(
-                        event: Event.finishSuccess(
-                            hex: hex
-                        )
-                    )
-                
-                isCompletingProfileSetup = false
-                root.updateViewState(showTabBarView: true)
-            } catch {
-                showAlert = AnyAppAlert(error: error)
-                logManager
-                    .trackEvent(
-                        event: Event.finishFail(
-                            error: error
-                        )
-                    )
-            }
-        }
-    }
-}
-
-// MARK: - Event
-private extension OnboardingCompletedView {
-    
-    enum Event: LoggableEvent {
-        case finishStart
-        case finishSuccess(hex: String)
-        case finishFail(error: Error)
-        
-        var eventName: String {
-            switch self {
-            case .finishStart: "OnboardingCompletedView_Finish_Start"
-            case .finishSuccess: "OnboardingCompletedView_Finish_Success"
-            case .finishFail: "OnboardingCompletedView_Finish_Fail"
-            }
-        }
-        
-        var parameters: [String: Any]? {
-            switch self {
-            case .finishSuccess(hex: let hex):
-                return [
-                    "profile_color_hex": hex
-                ]
-            case .finishFail(error: let error):
-                return error.eventParameters
-            default:
-                return nil
-            }
-        }
-        
-        var type: LogType {
-            switch self {
-            case .finishFail:
-                return .severe
-            default:
-                return .analytic
-            }
-        }
+        .showCustomAlert(alert: $viewModel.showAlert)
     }
 }
 
 #Preview {
-    OnboardingCompletedView(selectedColor: .mint)
-        .environment(UserManager(services: MockUserServices()))
-        .environment(AppState())
-        .previewEnvironment()
+    OnboardingCompletedView(
+        viewModel: OnboardingCompletedViewModel(
+            onboardingCompletedUseCase: OnboardingCompletedUseCase(
+                container: DevPreview
+                    .shared.container)
+        ),
+        selectedColor: .orange
+    )
+    .previewEnvironment()
 }
