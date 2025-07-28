369c9917b10af75a8915886a14be7555d44b99ed
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-28 20:44:03 +0300

    feat: implement `SettingsUseCase`

---
+++ AIChat/Core/Settings/SettingsUseCase.swift
@@ -0,0 +1,74 @@
+//
+//  SettingsUseCase.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 28.07.2025.
+//
+
+import Foundation
+
+@MainActor
+final class SettingsUseCase {
+    
+    private let authManager: AuthManager
+    private let userManager: UserManager
+    private let avatarManager: AvatarManager
+    private let chatManager: ChatManager
+    private let logManager: LogManager
+    
+    init(container: DependencyContainer) {
+        self.authManager = container.resolve(AuthManager.self)!
+        self.userManager = container.resolve(UserManager.self)!
+        self.avatarManager = container.resolve(AvatarManager.self)!
+        self.chatManager = container.resolve(ChatManager.self)!
+        self.logManager = container.resolve(LogManager.self)!
+    }
+}
+
+extension SettingsUseCase: SettingsUseCaseProtocol {
+    
+    var auth: UserAuthInfo? {
+        authManager.auth
+    }
+    
+    func signOut() throws {
+        try authManager.signOut()
+        userManager.signOut()
+    }
+    
+    func deleteAccount() async throws {
+        let userId = try authManager.getAuthId()
+        
+//                async let deleteAuth: () = authManager.deleteAccount()
+//                async let deleteUser: () = userManager.deleteCurrentUser()
+//                async let deleteAvatar: () = avatarManager
+//                    .removeAuthorIdFromAllUserAvatars(userId: userId)
+//                async let deleteChats: () = chatManager.deleteAllChatsForUser(
+//                    userId: userId
+//                )
+//
+//                let (_, _, _, _) = await (
+//                    try deleteAuth,
+//                    try deleteUser,
+//                    try deleteAvatar,
+//                    try deleteChats
+//                )
+        
+        try await chatManager.deleteAllChatsForUser(userId: userId)
+        
+        try await avatarManager
+            .removeAuthorIdFromAllUserAvatars(userId: userId)
+        
+        try await userManager.deleteCurrentUser()
+        
+        try await authManager.deleteAccount()
+        
+        // FIXME: implement
+        // try await purchaseManager.logOut()
+        logManager.deleteUserProfile()
+    }
+    
+    func trackEvent(event: any LoggableEvent) {
+        logManager.trackEvent(event: event)
+    }
+}
