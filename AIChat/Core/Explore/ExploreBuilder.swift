2b0bcb9149b9ac06c705923cdc6423a6019ad7e8
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-30 03:02:37 +0300

    feat: implement `ExploreBuilder`

---
+++ AIChat/Core/Explore/ExploreBuilder.swift
@@ -0,0 +1,26 @@
+//
+//  ExploreBuilder.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 30.07.2025.
+//
+
+import SwiftUI
+
+@Observable
+@MainActor
+final class ExploreBuilder {
+    private let container: DependencyContainer
+
+    init(container: DependencyContainer) {
+        self.container = container
+    }
+
+    func buildExploreView() -> some View {
+        ExploreView(
+            viewModel: ExploreViewModel(
+                interactor: CoreInteractor(container: container)
+            )
+        )
+    }
+}
