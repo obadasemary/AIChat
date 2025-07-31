a1f913b47135070174fac854647a61f174580fb0
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-31 15:59:04 +0300

    feat: implement `ChatDelegate`

---
+++ AIChat/Core/Chat/ChatDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  ChatDelegate.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 31.07.2025.
+//
+
+import Foundation
+
+struct ChatDelegate {
+    var avatarId: String
+    var chat: ChatModel?
+}
