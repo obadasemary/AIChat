de26b3b6778ae0f8b01116d4c43517b552ace347
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-31 05:08:32 +0300

    chore: update `openAIAPIKey`

--- AIChat/Utilities/Keys.swift
+++ AIChat/Utilities/Keys.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 struct Keys {
-    static let openAIAPIKey = "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"
+    static let openAIAPIKey = "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"
     static let mixpanelToken = "5b7313d016ebb60e3ac788b6a67474a8"
 }
