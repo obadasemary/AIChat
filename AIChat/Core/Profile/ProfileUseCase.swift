0cb35500998f0698817f6a5fa2b8a143462133a6
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-08-02 19:58:58 +0300

    feat: implement `ProfileUseCase`

---
+++ AIChat/Core/Profile/ProfileUseCase.swift
@@ -0,0 +1,47 @@
+//
+//  ProfileUseCase.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 02.08.2025.
+//
+
+import Foundation
+
+@MainActor
+final class ProfileUseCase {
+    
+    private let authManager: AuthManager
+    private let userManager: UserManager
+    private let avatarManager: AvatarManager
+    private let logManager: LogManager
+    
+    init(container: DependencyContainer) {
+        self.authManager = container.resolve(AuthManager.self)!
+        self.userManager = container.resolve(UserManager.self)!
+        self.avatarManager = container.resolve(AvatarManager.self)!
+        self.logManager = container.resolve(LogManager.self)!
+    }
+}
+
+extension ProfileUseCase: ProfileUseCaseProtocol {
+    
+    var currentUser: UserModel? {
+        userManager.currentUser
+    }
+    
+    func getAuthId() throws -> String {
+        try authManager.getAuthId()
+    }
+    
+    func getAvatarsForAuthor(userId: String) async throws -> [AvatarModel] {
+        try await avatarManager.getAvatarsForAuthor(userId: userId)
+    }
+    
+    func removeAuthorIdFromAvatar(avatarId: String) async throws {
+        try await avatarManager.removeAuthorIdFromAvatar(avatarId: avatarId)
+    }
+    
+    func trackEvent(event: any LoggableEvent) {
+        logManager.trackEvent(event: event)
+    }
+}
