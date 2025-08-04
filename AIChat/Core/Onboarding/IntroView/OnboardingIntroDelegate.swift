dc70a681aea9edac940fc45384005883908d48d4
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-08-04 04:27:53 +0300

    feat: implement `OnboardingIntroDelegate`

---
+++ AIChat/Core/Onboarding/IntroView/OnboardingIntroDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  OnboardingIntroDelegate.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 03.08.2025.
+//
+
+import SwiftUI
+
+struct OnboardingIntroDelegate {
+    var path: Binding<[OnboardingPathOption]>
+}
