78bd417ae681e1d3192f41b70ee8051fc89695f9
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-08-05 23:33:59 +0300

    chore: passing `devSettingsBuilder`, and `createAccountBuilder` as a `DI` into `ExploreBuilder`

--- AIChat/Core/Explore/ExploreBuilder.swift
+++ AIChat/Core/Explore/ExploreBuilder.swift
@@ -11,16 +11,30 @@ import SwiftUI
 @MainActor
 final class ExploreBuilder {
     private let container: DependencyContainer
+    private let devSettingsBuilder: DevSettingsBuilder
+    private let createAccountBuilder: CreateAccountBuilder
 
-    init(container: DependencyContainer) {
+    init(
+        container: DependencyContainer,
+        devSettingsBuilder: DevSettingsBuilder,
+        createAccountBuilder: CreateAccountBuilder
+    ) {
         self.container = container
+        self.devSettingsBuilder = devSettingsBuilder
+        self.createAccountBuilder = createAccountBuilder
     }
 
     func buildExploreView() -> some View {
         ExploreView(
             viewModel: ExploreViewModel(
                 exploreUseCase: ExploreUseCase(container: container)
-            )
+            ),
+            devSettingsView: {
+                self.devSettingsBuilder.buildDevSettingsView()
+            },
+            createAccountView: {
+                self.createAccountBuilder.buildCreateAccountView()
+            }
         )
     }
 }
