0e8060aac3c6ff95631f6e309292b0981e0b9d45
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-08-11 00:07:59 +0300

    chore: enhancement `PaywallView`

--- AIChat/Core/Paywall/PaywallView.swift
+++ AIChat/Core/Paywall/PaywallView.swift
@@ -13,51 +13,63 @@ struct PaywallView: View {
     @State var viewModel: PaywallViewModel
     
     @Environment(\.dismiss) private var dismiss
+    let option: PaywallOptional = .storeKit
     
     var body: some View {
-        ZStack {
-            if viewModel.products.isEmpty {
-                ProgressView()
-            } else {
-                CustomPaywallView(
-                    products: viewModel.products,
-                    onBackButtonPressed: {
-                        viewModel.onBackButtonPressed {
-                            dismiss()
-                        }
-                    },
-                    onRestorePurchasePressed: {
-                        viewModel.onRestorePurchasePressed {
-                            dismiss()
-                        }
-                    },
-                    onPurchaseProductPressed: { product in
-                        viewModel.onPurchaseProductPressed(product: product) {
-                            dismiss()
-                        }
+        Group {
+            switch option {
+            case .custom:
+                ZStack {
+                    if viewModel.products.isEmpty {
+                        ProgressView()
+                    } else {
+                        CustomPaywallView(
+                            products: viewModel.products,
+                            onBackButtonPressed: {
+                                viewModel.onBackButtonPressed {
+                                    dismiss()
+                                }
+                            },
+                            onRestorePurchasePressed: {
+                                viewModel.onRestorePurchasePressed {
+                                    dismiss()
+                                }
+                            },
+                            onPurchaseProductPressed: { product in
+                                viewModel.onPurchaseProductPressed(product: product) {
+                                    dismiss()
+                                }
+                            }
+                        )
+                    }
+                }
+            case .storeKit:
+                StoreKitPaywallView(
+                    productIds: viewModel.productIds,
+                    onInAppPurchaseStart: viewModel.onPurchaseStart,
+                    onInAppPurchaseCompletion: { (product, result) in
+                        viewModel
+                            .onPurchaseComplete(
+                                product: product,
+                                result: result
+                            ) {
+                                dismiss()
+                            }
                     }
                 )
             }
         }
-//        StoreKitPaywallView(
-//            productIds: viewModel.productIds,
-//            onInAppPurchaseStart: viewModel.onPurchaseStart,
-//            onInAppPurchaseCompletion: { (product, result) in
-//                viewModel
-//                    .onPurchaseComplete(
-//                        product: product,
-//                        result: result
-//                    ) {
-//                        dismiss()
-//                    }
-//            }
-//        )
         .screenAppearAnalytics(name: "Paywall")
         .showCustomAlert(alert: $viewModel.showAlert)
         .task {
             await viewModel.onLoadProducts()
         }
     }
+}
+
+enum PaywallOptional {
+    case custom
+    case storeKit
 }
 
 #Preview {
