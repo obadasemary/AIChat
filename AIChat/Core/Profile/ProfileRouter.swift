ab570550cdc127228079cbb9677570b7021c31ad
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-09-12 01:06:39 +0300

    feat: implement `ProfileRouter`

---
+++ AIChat/Core/Profile/ProfileRouter.swift
@@ -0,0 +1,41 @@
+//
+//  ProfileRouter.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 11.09.2025.
+//
+
+import SwiftUI
+import SUIRouting
+
+@MainActor
+protocol ProfileRouterProtocol {
+    func showCreateAvatarView()
+    func showSettingsView()
+}
+
+
+@MainActor
+struct ProfileRouter {
+    let router: Router
+    let settingsBuilder: SettingsBuilder
+    let createAvatarBuilder: CreateAvatarBuilder
+}
+    
+extension ProfileRouter: ProfileRouterProtocol {
+    
+    func showCreateAvatarView() {
+        router.showScreen(.fullScreenCover) { router in
+            createAvatarBuilder.buildCreateAvatarView()
+        }
+    }
+    
+    func showSettingsView() {
+        router.showScreen(.sheet) { router in
+            settingsBuilder.buildSettingsView()
+        }
+    }
+}
+
+//MARK: FIXME We don't need it just if we going to use CoreRouter and CoreBuilder
+extension CoreRouter: ProfileRouterProtocol {}
