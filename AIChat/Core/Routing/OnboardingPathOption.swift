7f1ee0d76b070c8c75eb0a5a8260d936c603abc1
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-10-06 22:31:43 +0300

    chore: remove/comment `OnboardingPathOption`

--- AIChat/Core/Routing/OnboardingPathOption.swift
+++ AIChat/Core/Routing/OnboardingPathOption.swift
@@ -1,61 +1,61 @@
+////
+////  OnboardingPathOption.swift
+////  AIChat
+////
+////  Created by Abdelrahman Mohamed on 29.07.2025.
+////
 //
-//  OnboardingPathOption.swift
-//  AIChat
+//import SwiftUI
 //
-//  Created by Abdelrahman Mohamed on 29.07.2025.
+//enum OnboardingPathOption: Hashable {
+//    case onboardingIntro
+//    case onboardingCommunity
+//    case onboardingColor
+//    case onboardingComplete(selectedColor: Color)
+//}
 //
-
-import SwiftUI
-
-enum OnboardingPathOption: Hashable {
-    case onboardingIntro
-    case onboardingCommunity
-    case onboardingColor
-    case onboardingComplete(selectedColor: Color)
-}
-
-struct NavigationDestinationForOnboardingModuleViewModifier: ViewModifier {
-    
-    @Environment(OnboardingIntroBuilder.self) private var onboardingIntroBuilder
-    @Environment(OnboardingCommunityBuilder.self) private var onboardingCommunityBuilder
-    @Environment(OnboardingColorBuilder.self) private var onboardingColorBuilder
-    @Environment(OnboardingCompletedBuilder.self) private var onboardingCompletedBuilder
-    let path: Binding<[OnboardingPathOption]>
-    
-    func body(content: Content) -> some View {
-        content
-            .navigationDestination(for: OnboardingPathOption.self) { newValue in
-                switch newValue {
-                case .onboardingIntro:
-                    onboardingIntroBuilder
-                        .buildOnboardingIntroView(
-                            delegate: OnboardingIntroDelegate(path: path)
-                        )
-                case .onboardingCommunity:
-                    onboardingCommunityBuilder
-                        .buildOnboardingCommunityView(
-                            delegate: OnboardingCommunityDelegate(path: path)
-                        )
-                case .onboardingColor:
-                    onboardingColorBuilder
-                        .buildOnboardingColorView(
-                            delegate: OnboardingColorDelegate(path: path)
-                        )
-                case .onboardingComplete(selectedColor: let selectedColor):
-                    onboardingCompletedBuilder
-                        .buildOnboardingCompletedView(
-                            delegate: OnboardingCompletedDelegate(
-                                selectedColor: selectedColor
-                            )
-                        )
-                }
-            }
-    }
-}
-
-extension View {
-    
-    func navigationDestinationForOnboardingModule(path: Binding<[OnboardingPathOption]>) -> some View {
-        modifier(NavigationDestinationForOnboardingModuleViewModifier(path: path))
-    }
-}
+//struct NavigationDestinationForOnboardingModuleViewModifier: ViewModifier {
+//    
+//    @Environment(OnboardingIntroBuilder.self) private var onboardingIntroBuilder
+//    @Environment(OnboardingCommunityBuilder.self) private var onboardingCommunityBuilder
+//    @Environment(OnboardingColorBuilder.self) private var onboardingColorBuilder
+//    @Environment(OnboardingCompletedBuilder.self) private var onboardingCompletedBuilder
+//    let path: Binding<[OnboardingPathOption]>
+//    
+//    func body(content: Content) -> some View {
+//        content
+//            .navigationDestination(for: OnboardingPathOption.self) { newValue in
+//                switch newValue {
+//                case .onboardingIntro:
+//                    onboardingIntroBuilder
+//                        .buildOnboardingIntroView(
+//                            delegate: OnboardingIntroDelegate(path: path)
+//                        )
+//                case .onboardingCommunity:
+//                    onboardingCommunityBuilder
+//                        .buildOnboardingCommunityView(
+//                            delegate: OnboardingCommunityDelegate()
+//                        )
+//                case .onboardingColor:
+//                    onboardingColorBuilder
+//                        .buildOnboardingColorView(
+//                            delegate: OnboardingColorDelegate()
+//                        )
+//                case .onboardingComplete(selectedColor: let selectedColor):
+//                    onboardingCompletedBuilder
+//                        .buildOnboardingCompletedView(
+//                            delegate: OnboardingCompletedDelegate(
+//                                selectedColor: selectedColor
+//                            )
+//                        )
+//                }
+//            }
+//    }
+//}
+//
+//extension View {
+//    
+//    func navigationDestinationForOnboardingModule(path: Binding<[OnboardingPathOption]>) -> some View {
+//        modifier(NavigationDestinationForOnboardingModuleViewModifier(path: path))
+//    }
+//}
