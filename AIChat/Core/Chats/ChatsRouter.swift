f91c6f9184c58d3705a41ada48d1c3e90c2d8389
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-10-16 23:35:48 +0300

    feat: implement `ChatsRouter`

---
+++ AIChat/Core/Chats/ChatsRouter.swift
@@ -0,0 +1,32 @@
+//
+//  ChatsRouter.swift
+//  AIChat
+//
+//  Created by Abdelrahman Mohamed on 16.10.2025.
+//
+
+import SwiftUI
+import SUIRouting
+
+@MainActor
+protocol ChatsRouterProtocol {
+    func showChatView(delegate: ChatDelegate)
+}
+
+@MainActor
+struct ChatsRouter {
+    let router: Router
+    let chatBuilder: ChatBuilder
+}
+
+extension ChatsRouter: ChatsRouterProtocol {
+    
+    func showChatView(delegate: ChatDelegate) {
+        router.showScreen(.push) { router in
+            chatBuilder.buildChatView(delegate: delegate)
+        }
+    }
+}
+
+//MARK: FIXME We don't need it just if we going to use CoreRouter and CoreBuilder
+extension CoreRouter: ChatsRouterProtocol {}
