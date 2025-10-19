6452911fa2a144549dc43ea8c026bf1d4b5a080f
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-10-20 01:14:10 +0300

    test(Profile): update MockProfileRouter to match protocol changes
    - Add showChatView(delegate:) method
    - Add showSimpleAlert(title:subtitle:) method
    - Update showCreateAvatarView to accept onDisappear callback
    - Add tracking properties for new methods and their parameters
    
    This fixes CI build failure where mock didn't conform to updated
    ProfileRouterProtocol from previous commit.

--- AIChatTests/Core/Profile/MockProfileRouter.swift
+++ AIChatTests/Core/Profile/MockProfileRouter.swift
@@ -13,12 +13,30 @@ final class MockProfileRouter: ProfileRouterProtocol {
     
     private(set) var showSettingsViewCalled = false
     private(set) var showCreateAvatarViewCalled = false
+    private(set) var showChatViewCalled = false
+    private(set) var showChatViewDelegate: ChatDelegate?
+    private(set) var showSimpleAlertCalled = false
+    private(set) var showSimpleAlertTitle: String?
+    private(set) var showSimpleAlertSubtitle: String?
+    private(set) var createAvatarOnDisappearCallback: (() -> Void)?
 
     func showSettingsView() {
         showSettingsViewCalled = true
     }
 
-    func showCreateAvatarView() {
+    func showCreateAvatarView(onDisappear: @escaping () -> Void) {
         showCreateAvatarViewCalled = true
+        createAvatarOnDisappearCallback = onDisappear
+    }
+    
+    func showChatView(delegate: ChatDelegate) {
+        showChatViewCalled = true
+        showChatViewDelegate = delegate
+    }
+    
+    func showSimpleAlert(title: String, subtitle: String?) {
+        showSimpleAlertCalled = true
+        showSimpleAlertTitle = title
+        showSimpleAlertSubtitle = subtitle
     }
 }
