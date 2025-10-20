31a16ab90464de1dcb0051ffb206b066878bc604
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-10-21 00:15:07 +0300

    chore: move `ChatUseCaseProtocol` into `ChatUseCase` instead

--- AIChat/Core/Chat/ChatUseCase.swift
+++ AIChat/Core/Chat/ChatUseCase.swift
@@ -8,6 +8,33 @@
 import Foundation
 
 @MainActor
+protocol ChatUseCaseProtocol {
+    
+    var currentUser: UserModel? { get }
+    var auth: UserAuthInfo? { get }
+    var isPremium: Bool { get }
+    
+    // Avatar
+    func getAuthId() throws -> String
+    func getAvatar(id: String) async throws -> AvatarModel?
+    func addRecentAvatar(avatar: AvatarModel) async throws
+    
+    // Chat
+    func getChat(userId: String, avatarId: String) async throws -> ChatModel?
+    func streamChatMessages(chatId: String) -> AsyncThrowingStream<[ChatMessageModel], any Error>
+    func markChatMessagesAsSeen(chatId: String, messageId: String, userId: String) async throws
+    func createNewChat(chat: ChatModel) async throws
+    func addChatMessage(message: ChatMessageModel) async throws
+    func reportChat(chatId: String, userId: String) async throws
+    func deleteChat(chatId: String) async throws
+    
+    // AI
+    func generateText(chats: [AIChatModel]) async throws -> AIChatModel
+    
+    func trackEvent(event: any LoggableEvent)
+}
+
+@MainActor
 final class ChatUseCase {
     
     private let authManager: AuthManager
