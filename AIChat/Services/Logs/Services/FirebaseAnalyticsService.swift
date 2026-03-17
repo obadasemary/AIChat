644c9703aa55f8e602816601f7a32ada200c972f
Abdelrahman Mohamed <obada.semary@gmail.com>
2026-03-17 19:49:46 +0300

    Import SamuraiLogging in log services

--- AIChat/Services/Logs/Services/FirebaseAnalyticsService.swift
+++ AIChat/Services/Logs/Services/FirebaseAnalyticsService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import FirebaseAnalytics
+import SamuraiLogging
 
 struct FirebaseAnalyticsService {}
 
--- AIChat/Services/Logs/Services/MixpanelService.swift
+++ AIChat/Services/Logs/Services/MixpanelService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Mixpanel
+import SamuraiLogging
 
 struct MixpanelService {
     
